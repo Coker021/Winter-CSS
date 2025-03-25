6f48f2441857eff43ffb976cfde33a706f96d338
Coker021 <106888492+Coker021@users.noreply.github.com>
2025-03-25 14:55:06 -0400

    Delete js directory

--- js/custom-js.js
+++
@@ -1,7 +0,0 @@
-$(window).on("scroll", function (){
-   if($(this).scrollTop() > 100){
-       $("header").addClass("scrolledHeader");
-   }else{
-       $("header").removeClass("scrolledHeader");
-   }
-});
